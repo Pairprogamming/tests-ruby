9eb554d1fe4a0cbf6b27019d04db46e6df9b951f
Ecris ton nom ici, en gardant les quotes <ndongogrego@gmail.com>
2019-01-14 16:41:12 +0100

    Ajout de l'exercice Basic

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,7 +1,35 @@
-def biggest (*values)
+def who_is_bigger (a,b,c)
 
-	values.max
+	if a == nil || b == nil || c == nil
+
+		return "nil detected"
+
+	elsif a >= b && a >= c
+
+		return "a is bigger"
+
+	elsif b >= a && b >= c
+
+		return "b is bigger"	
+
+	elsif c >= a && c >= b
+
+		return "c is bigger"
+	end
 
 end 
 
-#puts biggest(4,6)
+def reverse_upcase_noLTA(string)
+    string.reverse!
+    string.upcase!
+    string.delete! "L" "T" "A"
+    return string
+end
+
+
+
+def array_42(a)
+
+	return a.include?(42)
+
+end
